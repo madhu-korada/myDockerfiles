0c81a8ecd1e27dc445759ecd0bac2c9509e3c129
Madhu Korada <manoj.korada0000@gmail.com>
2023-04-06 22:59:41 -0400

    added run file in kimera

---
+++ kimera/docker_run.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+# Make sure processes in the container can connect to the x server
+# Necessary so gazebo can create a context for OpenGL rendering (even headless)
+XAUTH=/tmp/.docker.xauth
+if [ ! -f $XAUTH ]; then
+    xauth_list=$(xauth nlist $DISPLAY)
+    xauth_list=$(sed -e 's/^..../ffff/' <<<"$xauth_list")
+    if [ ! -z "$xauth_list" ]; then
+        echo "$xauth_list" | xauth -f $XAUTH nmerge -
+    else
+        touch $XAUTH
+    fi
+    chmod a+r $XAUTH
+fi
+
+DOCKER_OPTS="$DOCKER_OPTS --runtime=nvidia"
+
+# Prevent executing "docker run" when xauth failed.
+if [ ! -f $XAUTH ]; then
+    echo "[$XAUTH] was not properly created. Exiting..."
+    exit 1
+fi
+
+BASH_OPTION=bash
+docker run \
+    -it \
+    -e DISPLAY \
+    -e QT_X11_NO_MITSHM=1 \
+    -e XAUTHORITY=$XAUTH \
+    -v "$XAUTH:$XAUTH" \
+    -v "/tmp/.X11-unix:/tmp/.X11-unix" \
+    -v "/etc/localtime:/etc/localtime:ro" \
+    -v "/dev:/dev" \
+    -v /run/udev:/run/udev \
+    -v "/var/run/docker.sock:/var/run/docker.sock" \
+    --name locobot-habitat \
+    --network host \
+    --privileged \
+    --security-opt seccomp=unconfined \
+    $DOCKER_OPTS \
+    kimera_vio_semantics:GPU \
+    $BASH_OPTION
